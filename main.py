4d7deb979cad2bc2901e011d0664317b7685b434
Chichibang <112266878+Chichibang@users.noreply.github.com>
2023-08-27 22:17:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import os
+import getpass
+config = open("~/terminal-plus/term.cfg", "r")
+config = config.read()
+exec(config)
 os.system("clear")
+if logintoadifferentaccount == True:
+    os.system("su ", account)
 print("Welcome to Terminal+")
 while True:
+    if safemode == True:
+        username = getpass.getuser()
+        if username == "root":
+            exit
     currentdir = os.getcwd()
     cmdexec = input(currentdir, ">")
-    os.system(cmdexec)
+    if autosuperuser == True:
+        os.system("sudo ", cmdexec)
+    else:
+        os.system(cmdexec)
     
